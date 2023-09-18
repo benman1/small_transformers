074e33f378df4d6df77b4986ea549b81d697ff0c
benman1 <auffarth@gmail.com>
2023-09-18 15:07:45 +0100

    tests for multiplicative and mha attention

--- small_transformer/model/normalization.py
+++ small_transformer/model/normalization.py
@@ -1,19 +1,16 @@
 """Normalization functionality."""
 import torch
-from torch import nn
+from torch import nn, Tensor
 
 from small_transformer.base import ModelBase
 
 
 class LayerNorm(ModelBase):
-    """Z-normalization.
-
-    Gamma and beta are learnable parameters that scale and shift.
-    """
-    def __init__(self, d_model, eps=1e-6):
+    """Z-normalization."""
+    def __init__(self, d_model, eps: float = 1e-6):
         super().__init__()
-        self.gamma = nn.Parameter(torch.ones(d_model))
-        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.weight = nn.Parameter(torch.ones(d_model))
+        self.bias = nn.Parameter(torch.zeros(d_model))
         self.eps = eps
 
     def forward(self, x):
@@ -21,7 +18,7 @@ class LayerNorm(ModelBase):
         std = x.std(-1, keepdim=True)
 
         out = (x - mean) / (std + self.eps)
-        out = self.gamma * out + self.beta
+        out = self.weight * out + self.bias
 
         return out
 
@@ -35,11 +32,15 @@ class RMSNorm(ModelBase):
     Reference:
     Root mean square layer normalization by B. Zhang and R. Sennrich (2019).
     """
-    def __init__(self, d_model, eps=1e-6):
+    def __init__(self, in_features: int, eps: float = 1e-6):
         super().__init__()
         self.eps = eps
+        self.weight = nn.Parameter(torch.ones(in_features))
 
-    def forward(self, x):
-        rms = x.pow(2).sum(-1, keepdim=True).sqrt()
-        x = x / (rms + self.eps)
-        return x
+    def get_norm(self, x: Tensor):
+        """Calculate the norm."""
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x: Tensor):
+        normed = self.get_norm(x.float())
+        return normed.type_as(x)
